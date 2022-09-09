8a325196fc6d6c2b9114f6f065a0b95ff7379d81
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-09-09 17:23:07 +0200

    Update register.go

--- pkg/client/clientset/versioned/scheme/register.go
+++ pkg/client/clientset/versioned/scheme/register.go
@@ -16,7 +16,7 @@ limitations under the License.
 package scheme
 
 import (
-	kaosv1 "github.com/arnaudmz/kaos/pkg/apis/kaos/v1"
+	kaosv1 "github.com/DjaloS/kaos/pkg/apis/kaos/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
