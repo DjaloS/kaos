f19ade2ffc7825e5e2cb4fa4cdf0a627d012adec
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-09-11 14:09:58 +0200

    Update factory_interfaces.go

--- pkg/client/informers/externalversions/internalinterfaces/factory_interfaces.go
+++ pkg/client/informers/externalversions/internalinterfaces/factory_interfaces.go
@@ -21,7 +21,7 @@ package internalinterfaces
 import (
 	time "time"
 
-	versioned "github.com/arnaudmz/kaos/pkg/client/clientset/versioned"
+	versioned "github.com/DjaloS/kaos/pkg/client/clientset/versioned"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	cache "k8s.io/client-go/tools/cache"
