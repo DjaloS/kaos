8fc88f0c1751e37ca0aada882c396a3b72ec8d9d
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-09-11 14:06:54 +0200

    Update generic.go

--- pkg/client/informers/externalversions/generic.go
+++ pkg/client/informers/externalversions/generic.go
@@ -21,7 +21,7 @@ package externalversions
 import (
 	"fmt"
 
-	v1 "github.com/arnaudmz/kaos/pkg/apis/kaos/v1"
+	v1 "github.com/DjaloS/kaos/pkg/apis/kaos/v1"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	cache "k8s.io/client-go/tools/cache"
 )
