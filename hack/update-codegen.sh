2424635c5dfb344b23c1f960544710f9ca5dd0e3
DjaloS <109952373+DjaloS@users.noreply.github.com>
2022-09-11 14:17:47 +0200

    Update update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -22,5 +22,5 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ${GOPATH}/src/k8s.io/code-generator)}
 
 vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/arnaudmz/kaos/pkg/client github.com/arnaudmz/kaos/pkg/apis \
+  github.com/DjaloS/kaos/pkg/client github.com/DjaloS/kaos/pkg/apis \
   kaos:v1 --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
